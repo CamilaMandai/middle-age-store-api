1e02e3462114d59703077db4233c899ded68d956
Camila Mandai <cayumandai@gmail.com>
2023-02-07 16:20:17 -0300

    fix: corrige condições de middleware de users

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -4,11 +4,11 @@ import { NextFunction, Response, Request } from 'express';
 
 const usernameToCreate = async (req : Request, res : Response, next: NextFunction) => {
   const { username } = req.body;
-  if (!username) return res.status(400).json({ message: '"name" is required' });
+  if (!username) return res.status(400).json({ message: '"username" is required' });
   if (typeof username !== 'string') {
     return res.status(422).json({ message: '"username" must be a string' });
   }
-  if (username.length < 4) {
+  if (username.length < 2) {
     return res.status(422)
       .json({ message: '"username" length must be at least 3 characters long' });
   }
@@ -17,7 +17,7 @@ const usernameToCreate = async (req : Request, res : Response, next: NextFunctio
 
 const passwordToCreate = async (req : Request, res : Response, next: NextFunction) => {
   const { password } = req.body;
-  if (!password) return res.status(400).json({ message: '"name" is required' });
+  if (!password) return res.status(400).json({ message: '"password" is required' });
   if (typeof password !== 'string') { 
     return res.status(422).json({ message: '"password" must be a string' });
   }
@@ -31,12 +31,12 @@ const passwordToCreate = async (req : Request, res : Response, next: NextFunctio
 const levelToCreate = async (req : Request, res : Response, next: NextFunction) => {
   const { level } = req.body;
   if (!level) return res.status(400).json({ message: '"level" is required' });
-  if (typeof level !== 'string') { 
+  if (typeof level !== 'number') { 
     return res.status(422).json({ message: '"level" must be a number' });
   }
-  if (level.length < 1) {
+  if (level < 1) {
     return res.status(422)
-      .json({ message: '"level" length must be greater than or equal to 1' });
+      .json({ message: '"level" must be greater than or equal to 1' });
   }
   return next();
 };
