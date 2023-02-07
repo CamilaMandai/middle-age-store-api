9054cbd324639b9829915a895fa9035166c1a41a
Camila Mandai <cayumandai@gmail.com>
2023-02-07 16:42:04 -0300

    fix: corrige condições de middleware de users que checa level de novo

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -8,7 +8,7 @@ const usernameToCreate = async (req : Request, res : Response, next: NextFunctio
   if (typeof username !== 'string') {
     return res.status(422).json({ message: '"username" must be a string' });
   }
-  if (username.length < 2) {
+  if (username.length < 4) {
     return res.status(422)
       .json({ message: '"username" length must be at least 3 characters long' });
   }
@@ -30,11 +30,11 @@ const passwordToCreate = async (req : Request, res : Response, next: NextFunctio
 
 const levelToCreate = async (req : Request, res : Response, next: NextFunction) => {
   const { level } = req.body;
-  if (!level) return res.status(400).json({ message: '"level" is required' });
+  if (!level && level !== 0) return res.status(400).json({ message: '"level" is required' });
   if (typeof level !== 'number') { 
     return res.status(422).json({ message: '"level" must be a number' });
   }
-  if (level < 0) {
+  if (level <= 0) {
     return res.status(422)
       .json({ message: '"level" must be greater than or equal to 1' });
   }
